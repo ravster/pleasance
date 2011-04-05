c43caf3ff63f3c75587d846487782bf79cfbde2c
Ravi Desai <ravi@ravi-laptop.localdomain>
2011-04-05 13:52:32 +0000

    * data-mgmt: ** Reduced bar-array to 4000 exemplars since we don't seem to need the full thing.  Reduce the size to use less space and make the program have to run through less. ** Limit the loop to do only filling up the bar-array.  Ignore the rest of the data that we have.  Reasons: efficiency. * array-nn-impl.lisp:   This is a file I've been working on for months.  Of course, I've not done anything in the last month since I was waiting for the Yu+Wang+Lai book.  I thought about making the weights and nodes in arrays since they would then be automatically extendible by the program, and I would not have to manually create new symbols for them, which was getting quite tedious.  I'm glad I thought of this, even though it was after spending a few days trying to figure out some way to manipulate lists and symbols and what-not.   I had made macros for the hidden-node and output-node creation, but then I found them difficult to patch on when I re-refactored the code for use by GAs. * nn-for-ga.lisp:   This is a refactoring of the array-nn-impl file to place everything I can into one function called "NN".  This function receives a chromosome from the GA, creates and evaluates the resulting NN, and then gives the GA the results of evaluating the trained NN on the training and testing sets.  It stops training the GA when we hit 2000 epochs, or when the validation-error begins to rise, whichever comes first. * Notes: ** I'm going to try to make as much of the program in a functional style.    Over the last month, I've read about Erlang, and parallel programming.  I've also gone through the Yu+Wang+Lai book "Foreign-exchange-rate forecasting with artificial neural networks".    The authors suggest that the best way to find out which inputs are useful is to use genetic algorithms (GAs).  This means that I'll be evaluating multiple NNs against each other.  I don't want to do this manually.  I will automate this, and that means getting the NNs to be more functional. ** I'm going to use smaller datasets.    The authors use 3-4 years of training data of the daily type for a lot of their examples.  Along with 1 year of testing data, this comes around to <2000 exemplars, which is good since it'll make training and testing a lot quicker.  I like this. ** I'm going to have to make more indicators    This is because I'm going to be testing a lot of indicators against each other.  I'm going to make them in as much of a functional style as I can.  I should functional-ize my program in a systematic manner, in the order that the program is loaded. * Problems:   Currently the array-nn-impl has aggregate errors of <15 while the NN-function (For use with the GA) has an error of >100.  This should not be happening; they should be equivalent.

---
+++ array-nn-impl.lisp
@@ -0,0 +1,111 @@
+;; Set the package
+(in-package :ravi.nn0)
+
+;; State number of input and hidden nodes
+(defparameter numberofinputnodes 3)
+(defparameter numberofhiddennodes 7)
+
+;; Define weights for 1st interlevel (Between input and hidden nodes)
+(defparameter weights-1 (make-array (list numberofinputnodes numberofhiddennodes)))
+(defun define-weights-1 ()
+  (loop for i below (array-dimension weights-1 0) do
+       (loop for j below (array-dimension weights-1 1) do
+	    (setf (aref weights-1 i j) (random 3)))))
+(define-weights-1)
+
+;; Define weights for 2nd interlevel
+(defparameter weights-2 (make-array numberofhiddennodes))
+(defun define-weights-2 ()
+  (loop for i below (length weights-2) do
+       (setf (aref weights-2 i) (random 3))))
+(define-weights-2)
+
+;; Define rate of learning
+(defparameter rate-of-learning (/ (1+ (random 10))
+				     1000))
+
+;; Define input-nodes
+(defparameter input-node (make-array numberofinputnodes))
+
+(defun define-input-nodes (list-of-set-indexes)
+  (loop for j below (length input-node)
+     do
+     (let ((i (elt list-of-set-indexes j)))
+       (setf (aref input-node j)
+	     (lambda (dataset input-index)
+	       (aref dataset input-index i))))))
+
+(define-input-nodes '(2 1 3))
+
+;; Define hidden nodes
+(defparameter hidden-node (make-array numberofhiddennodes))
+
+(defun define-hidden-nodes ()
+  (loop for i below numberofhiddennodes do
+       (let ((hidden-node-index i) ;Because we are closing over this variable for each hidden-node.
+	     (number-of-input-nodes numberofinputnodes))
+	 (setf (aref hidden-node hidden-node-index)
+	       (lambda (dataset input-index)
+		 (tanh
+		  (loop for input-node-index below number-of-input-nodes sum
+		       (* (aref weights-1 input-node-index hidden-node-index)
+			  (funcall (aref input-node input-node-index)
+				   dataset input-index)))))))))
+
+(define-hidden-nodes)
+
+;; Define output node.  Same name and parameters used right now so that it plugs into the rest of the code.
+
+(defmacro output-node ()
+  (let ((a ()))
+    (loop for i below numberofhiddennodes do
+	 (push `(* (aref weights-2 ,i)
+		   (funcall (aref hidden-node ,i) dataset input-index))
+	       a))
+    `(tanh (+ ,@a)))) 
+
+(defun node-output (dataset input-index)
+  (tanh
+   (loop for i below numberofhiddennodes sum (* (aref weights-2 i) 
+						(funcall (aref hidden-node i) dataset input-index)))))
+
+;; The following code is used for training the NN.
+(defparameter hneg (make-array numberofhiddennodes))
+(defun error-gradient-of-output-node (dataset input-index)
+  ""
+  (* (- 1 (expt (node-output dataset input-index)
+		2))			     ;1 - y^2
+     (- (aref training-set input-index 2)    ;Answer we want
+	(node-output dataset input-index)))) ;Answer we have right now.
+
+(defun work-horse (dataset)
+  (loop for exemplar below (array-dimension dataset 0)
+       with output-node-error-gradient = 0
+       do
+       (setf output-node-error-gradient (error-gradient-of-output-node dataset exemplar))
+     ;; Calc hiddennode error gradients.  For use with updating weights-1
+       (loop for i below (length hidden-node)	;For all hidden nodes.
+	    do
+	    (setf (aref hneg i)
+		  (* (aref weights-2 i)	;Weight of the hidden node
+		     (- 1 (expt (funcall (aref hidden-node i) dataset exemplar)
+				2))	;Derivative of output of hidden node.
+		     output-node-error-gradient)))
+
+     ;; Update weights-1
+       (loop for input-node-iterator below (array-dimension weights-1 0) do
+	    (loop for hidden-node-iterator below (array-dimension weights-1 1) do
+		 (incf (aref weights-1 input-node-iterator hidden-node-iterator)
+		       (* rate-of-learning
+			  (funcall (aref input-node input-node-iterator) dataset exemplar)
+			  (aref hneg hidden-node-iterator)))))
+
+     ;; Update weights-2.
+     ;; We do this after weights-1 to maintain integrity.  Weights-1 is changed on the old weights-2 values, and weights-2 does not depend upon weights-1.
+       (loop for i below (length hidden-node)
+	    do
+	    (incf (aref weights-2 i)
+		  (* rate-of-learning
+		     (funcall (aref hidden-node i) dataset exemplar) ;Output of hidden node.
+		     output-node-error-gradient)))
+       ))
---
+++ nn-for-ga.lisp
@@ -0,0 +1,118 @@
+;;;; This is an NN module for use by the GA.  We give the NN module a chromosome of binary genes defining which input-vectors to use.  The NN module creates a NN, trains it, and then returns the error for the training and the testing sets back to the GA
+;; This will be all in one defun, and I will be using labels to have locally-defined functions.  I will be making arrays of the weights and nodes to make it flexible from iteration to iteration of the GA.
+
+(in-package :ravi.nn0)
+
+(defun nn (chromosome)
+  (let* ((numberofinputnodes (loop for i in chromosome count (= i 1)))
+	 (numberofhiddennodes (* 2 numberofinputnodes))
+	 (weights-1 (make-array (list numberofinputnodes numberofhiddennodes)))
+	 (weights-2 (make-array numberofhiddennodes))
+	 (rate-of-learning (/ 3 1000))
+	 (input-node (make-array numberofinputnodes))
+	 (hidden-node (make-array numberofhiddennodes))
+	 (hneg (make-array numberofhiddennodes)))
+    (labels ((error-gradient-of-output-node (dataset input-index)
+	       (* (- 1 (expt (output-node dataset input-index)
+			     2))		       ;1 - y^2
+		  (- (aref training-set input-index 2) ;Answer we want
+		     (output-node dataset input-index)))) ;Answer we have right now.
+	     (output-node (dataset input-index)
+	       (tanh
+		(loop for i below numberofhiddennodes sum (* (aref weights-2 i) 
+							     (funcall (aref hidden-node i) dataset input-index)))))
+	     (work-horse (dataset)
+	       (loop for exemplar below (array-dimension dataset 0) ;For the length of the dataset.
+		  with output-node-error-gradient = 0
+		  do
+		  (setf output-node-error-gradient (error-gradient-of-output-node dataset exemplar))
+		  ;; Calc hiddennode error gradients.  For use with updating weights-1
+		  (loop for i below (length hidden-node) ;For all hidden nodes.
+		     do
+		     (setf (aref hneg i)
+			   (* (aref weights-2 i) ;Weight of the hidden node
+			      (- 1 (expt (funcall (aref hidden-node i) dataset exemplar)
+					 2)) ;Derivative of output of hidden node.
+			      output-node-error-gradient)))
+
+		  ;; Update weights-1
+		  (loop for input-node-iterator below (array-dimension weights-1 0) do
+		       (loop for hidden-node-iterator below (array-dimension weights-1 1) do
+			    (incf (aref weights-1 input-node-iterator hidden-node-iterator)
+				  (* rate-of-learning
+				     (funcall (aref input-node input-node-iterator) dataset exemplar)
+				     (aref hneg hidden-node-iterator)))))
+
+		  ;; Update weights-2.
+		  ;; We do this after weights-1 to maintain integrity.  Weights-1 is changed on the hneg of the old weights-2 values, and weights-2 does not depend upon weights-1.
+		  (loop for i below (length hidden-node)
+		     do
+		     (incf (aref weights-2 i)
+			   (* rate-of-learning
+			      (funcall (aref hidden-node i) dataset exemplar) ;Output of hidden node.
+			      output-node-error-gradient)))
+		  ))			;End work-horse definition.
+	     (chromosome-to-input-vectors (chromosome)
+	       (let ((a ()))
+		 (loop for i below (length chromosome) do
+		      (if (= 1 (elt chromosome i))
+			  (push (1+ i)
+				a)))
+		 a))
+	     (define-input-nodes (list-of-set-indexes)
+	       (loop for j below (length input-node)
+		  do
+		  (let ((i (elt list-of-set-indexes j)))
+		    (setf (aref input-node j)
+			  (lambda (dataset input-index)
+			    (aref dataset input-index i))))))
+	     (define-hidden-nodes ()
+	       (loop for i below numberofhiddennodes do
+		    (let ((hidden-node-index i) ;Because we are closing over this variable for each hidden-node.
+			  (number-of-input-nodes numberofinputnodes))
+		      (setf (aref hidden-node hidden-node-index)
+			    (lambda (dataset input-index)
+			      (tanh
+			       (loop for input-node-index below number-of-input-nodes sum
+				    (* (aref weights-1 input-node-index hidden-node-index)
+				       (funcall (aref input-node input-node-index)
+						dataset input-index)))))))))
+	     (aggregate-error-in-set (dataset)
+	       "This function will find out what the total error is in the training set compared to the currect network output."
+	       (loop for i from 0 below (array-dimension dataset 0)
+		  sum (abs (- (aref dataset i 0) ;Answer we want
+			      (output-node dataset i))))) ;Answer we have right now.
+	     )				;End labels definition
+	
+      ;; Create the input-nodes
+      (define-input-nodes (chromosome-to-input-vectors chromosome))
+
+      ;; Create the hidden-nodes
+      (define-hidden-nodes)
+
+      ;; Define weights-1; the first interlevel
+      (loop for i below (array-dimension weights-1 0) do
+	   (loop for j below (array-dimension weights-1 1) do
+		(setf (aref weights-1 i j) (random 3))))
+
+      ;; Define weights-2; the second interlevel
+      (loop for i below (length weights-2) do
+	   (setf (aref weights-2 i) (random 3)))
+
+      ;; Train the network till the validation-set error begins to go up.
+      (work-horse training-set)
+      (let ((previous-validation-error (aggregate-error-in-set validation-set)))
+	(loop for i below 2000
+	   while (<= (aggregate-error-in-set validation-set)
+		     previous-validation-error) 
+	   finally (values
+		    (aggregate-error-in-set training-set)
+		    (aggregate-error-in-set test-set))
+	     do
+	   (work-horse training-set)
+	   (setf previous-validation-error (aggregate-error-in-set validation-set))))
+
+      
+	
+      ))
+  )			       ;End labels, macrolet, let*, and defun.
