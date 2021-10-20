f81252795aec0272d62607c77a835ddd6cfdf4eb
Ravi Desai <ravi@ravidesai.com>
2021-10-19 20:46:50 -0400

    moar procedures

--- java/Pleasance.java
+++ java/Pleasance.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.lang.Math;
 
 class Pleasance {
     static ArrayList<String> dates = new ArrayList<String>(200);
@@ -30,6 +31,43 @@ class Pleasance {
 	}
 	csvReader.close();
 
-	System.out.println(volumes);
+	System.out.println("Row count=" + volumes.size());
+
+	dollarCostAverage();
+    }
+
+    static void dollarCostAverage() {
+	System.out.println("Basic dollar-cost-averaging");
+
+	double leftover = 0;
+	double tot_bought = 0;
+	double tot_spent = 0;
+	int count = volumes.size();
+
+	for (int i = 0; i < count; i += 10) {
+	    double close = closes.get(i);
+	    double spendable = 50 + leftover;
+	    double bought = Math.floor(spendable/close);
+	    tot_bought += bought;
+	    double spent = bought * close;
+	    tot_spent += spent;
+	    leftover = spendable - spent;
+	}
+
+	System.out.println("Leftover=" + leftover + "\n bought=" + tot_bought +
+			   "\nSpent = " + tot_spent +
+			   "\naverage price bought=" + tot_spent / tot_bought +
+			   "\nMedian price = " + median(closes));
+	System.out.println("DONE dollar-cost-averaging");
+    }
+
+    static Double median(ArrayList<Double> data) {
+	ArrayList<Double> d2 = (ArrayList<Double>) data.clone();
+	d2.sort(null);
+	int index = (int)(data.size() / 2);
+	if ((data.size()/2) % 2 != 0) {
+	    return data.get(index);
+	}
+	return (data.get(index -1) + data.get(index)) / 2;
     }
 }
