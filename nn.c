cc4dd87cabb08b56bb18a233c58dd1acff2aec18
Ravi Desai <ravi@ravidesai.com>
2023-03-11 17:50:25 -0500

    Calculate the error-gradient for mid and output nodes

--- nn.c
+++ nn.c
@@ -132,10 +132,19 @@ int main (int argc, char** argv) {
   dates = malloc(num_rows * sizeof(char*));
   num_rows = load_ohlc(argv[1]);
 
-  calc_true_range(num_rows);
-  calc_atr_10(num_rows);
   float min = 0;
   float max = 0;
+  calc_min_max(&min, &max, closes, num_rows, 0, num_rows);
+  float* normalizedCloses = (float*) malloc(num_rows * sizeof(float));
+  for(int i = 20; i < num_rows; i++) {
+    float out = normalize_0_1(closes[i], min, max - min);
+    normalizedCloses[i] = out;
+  }
+
+  calc_true_range(num_rows);
+  calc_atr_10(num_rows);
+  min = 0;
+  max = 0;
   calc_min_max(&min, &max, atr10s, num_rows, 10, num_rows);
   float* normalizedAtr10s = (float*) malloc(num_rows * sizeof(float));
   for(int i = 10; i < num_rows; i++) {
@@ -178,10 +187,63 @@ int main (int argc, char** argv) {
   for(int i = 0; i < (num_out_nodes * num_mid_nodes); i++) {
     weights_mid_out[i] = drand48();
   }
+  float* mid_nodes = (float*) malloc(sizeof(float) * num_mid_nodes);
+  for(int i = 0; i < num_mid_nodes; i++) {
+    mid_nodes[i] = 0;
+  }
+  float* out_nodes = (float*) malloc(sizeof(float) * num_out_nodes);
+  for(int i = 0; i < num_out_nodes; i++) {
+    out_nodes[i] = 0;
+  }
 
-/* TODO:
-    - Move on to building a basic backpropagation NN that handles those to inputs and
-    the one output. KISS */
+  for(int i = 20; i < (num_rows - 15); i++) {
+    // Don't bother with a training vs testing dataset right now.  Just have something run.
+    // inputs
+    float atr10 = normalizedAtr10s[i];
+    float ma20 = normalizedMA20s[i];
+
+    // outputs
+    float closePlus15s = normalizedClosePlus15s[i];
+
+    // Calc output of each mid-node
+    for(int j = 0; j < num_mid_nodes; j++) {
+      float sum = 0;
+      sum += atr10 * weights_in_mid[(j * num_in_nodes) + 0];
+      sum += ma20 * weights_in_mid[(j * num_in_nodes) + 1];
+      // TODO same with closing price here.
+      mid_nodes[j] = tanh(sum);
+    }
+
+    // Calc output nodes
+    // TODO check this is row-first.  Does it even matter?
+    for(int j = 0; j < num_out_nodes; j++) {
+      float sum = 0;
+      for(int k = 0; k < num_mid_nodes; k++) {
+	sum += mid_nodes[k] * weights_mid_out[(j * num_mid_nodes) + 0];
+      }
+      out_nodes[j] = tanh(sum);
+    }
+
+    // Calc err for out-nodes
+    // TODO capable of multiple output nodes
+    float y = out_nodes[0];
+    float output_node_err_gradient = (1 - y*y) * (closePlus15s - y);
+
+    // TODO allow many out-nodes
+    // Calc err for mid-nodes
+    float *mid_nodes_error_gradients = (float*)malloc(sizeof(float) * num_mid_nodes);
+    for(int j = 0; j < num_mid_nodes; j++) {
+      float mid_node_output = mid_nodes[j];
+      mid_nodes_error_gradients[j] = weights_mid_out[j] *
+	(1 - mid_node_output * mid_node_output) *
+	output_node_err_gradient;
+    }
+    printf("%f\t%f\t%f\t%f\n", mid_nodes_error_gradients[0], mid_nodes_error_gradients[1], mid_nodes_error_gradients[2], mid_nodes_error_gradients[3]);
+
+    // TODO:
+    // Change weights to mid-nodes
+    // Change weights to out-nodes
+  }
 
   printf("done\n");
 }
